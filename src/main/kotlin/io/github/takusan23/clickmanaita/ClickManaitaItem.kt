f77e391a7621f4d3a2797a96bdec9dd67bada937
takusan23 <takumu1200@gmail.com>
2021-06-13 12:09:03 +0900

    クリエイティブタブ実装

--- src/main/kotlin/io/github/takusan23/clickmanaita/ClickManaitaItem.kt
+++ src/main/kotlin/io/github/takusan23/clickmanaita/ClickManaitaItem.kt
@@ -9,9 +9,25 @@ import net.minecraft.item.ItemStack
 import net.minecraft.screen.slot.Slot
 import net.minecraft.util.ClickType
 
+/** クリックまな板　アイテム */
 object ClickManaitaItem {
 
-    /** 右クリックしたらアイテムが増える */
-    val CLICKMANAITA_WOOD = ClickManaitaBaseItem(settings = FabricItemSettings().group(ItemGroup.TOOLS), dropSize = 2)
+    /** 木製。2個増える */
+    val CLICKMANAITA_WOOD = ClickManaitaBaseItem(settings = FabricItemSettings().group(ClickManaitaItemGroup.CREATIVE_TAB), dropSize = 2)
+
+    /** 石製。4個増える */
+    val CLICKMANAITA_STONE = ClickManaitaBaseItem(settings = FabricItemSettings().group(ClickManaitaItemGroup.CREATIVE_TAB), dropSize = 4)
+
+    /** 鉄製。8個増える */
+    val CLICKMANAITA_IRON = ClickManaitaBaseItem(settings = FabricItemSettings().group(ClickManaitaItemGroup.CREATIVE_TAB), dropSize = 8)
+
+    /** 金製。16個増える */
+    val CLICKMANAITA_GOLD = ClickManaitaBaseItem(settings = FabricItemSettings().group(ClickManaitaItemGroup.CREATIVE_TAB), dropSize = 16)
+
+    /** ダイヤ製。32個増える */
+    val CLICKMANAITA_DIAMOND = ClickManaitaBaseItem(settings = FabricItemSettings().group(ClickManaitaItemGroup.CREATIVE_TAB), dropSize = 32)
+
+    /** エメラルド製。64個増える */
+    val CLICKMANAITA_EMERALD = ClickManaitaBaseItem(settings = FabricItemSettings().group(ClickManaitaItemGroup.CREATIVE_TAB), dropSize = 64)
 
 }
