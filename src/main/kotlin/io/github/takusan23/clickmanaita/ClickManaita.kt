245ee83ee179be600f8423d0b7ccc43af5045a22
takusan23 <takumu1200@gmail.com>
2021-06-14 15:14:30 +0900

    Fabricのバージョンを上げるなど

--- src/main/kotlin/io/github/takusan23/clickmanaita/ClickManaita.kt
+++ src/main/kotlin/io/github/takusan23/clickmanaita/ClickManaita.kt
@@ -5,14 +5,11 @@ import io.github.takusan23.clickmanaita.item.ClickManaitaItem
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
 
-// For support join https://discord.gg/v6v4pMv
-
+/**
+ * エントリーポイント。起動時にinit関数が呼ばれる
+ * */
 @Suppress("unused")
 fun init() {
-    // This code runs as soon as Minecraft is in a mod-load-ready state.
-    // However, some things (like resources) may still be uninitialized.
-    // Proceed with mild caution.
-
     // アイテム追加
     Registry.register(Registry.ITEM, Identifier("clickmanaita", "clickmanaita_wood"), ClickManaitaItem.CLICKMANAITA_WOOD)
     Registry.register(Registry.ITEM, Identifier("clickmanaita", "clickmanaita_stone"), ClickManaitaItem.CLICKMANAITA_STONE)
