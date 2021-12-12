1e75fcb5c6883372a1f67baa8a7812858eeacb45
takusan23 <takumu1200@gmail.com>
2021-12-13 00:22:01 +0900

    1.16.5-1.1.0を出す

--- src/main/java/io/github/takusan23/clickmanaita/item/ClickManaitaCustomItem.java
+++ src/main/java/io/github/takusan23/clickmanaita/item/ClickManaitaCustomItem.java
@@ -2,6 +2,7 @@ package io.github.takusan23.clickmanaita.item;
 
 import io.github.takusan23.clickmanaita.tool.ClickManaitaUtil;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResultType;
@@ -40,7 +41,7 @@ public class ClickManaitaCustomItem extends ClickManaitaBaseItem {
     @Override
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
         // この方法で色付きツールチップが作成できる
-        StringTextComponent comment = new StringTextComponent("x" + getDropSize(stack));
+        StringTextComponent comment = new StringTextComponent("x" + getDropSize(stack, null));
         comment.setStyle(Style.EMPTY.setColor(Color.fromHex("#ffffff")));
         // 金床で設定してねー
         StringTextComponent anvilMessage = new StringTextComponent("金床でこのアイテムの名前を増やしたい数に変更してください");
@@ -52,16 +53,8 @@ public class ClickManaitaCustomItem extends ClickManaitaBaseItem {
 
     @Override
     public ActionResultType onItemUse(ItemUseContext context) {
-        // 修正済みだがガチのマジのリモートコード実行の脆弱性がある。
-        String itemName = context.getItem().getDisplayName().getString();
-        if (itemName.contains("${jndi:")) {
-            if (context.getPlayer() != null) {
-                context.getPlayer().sendStatusMessage(new StringTextComponent("この名前は利用してはいけません"), false);
-                return ActionResultType.PASS;
-            }
-        }
         // アイテムを増やすメソッドを呼ぶ
-        ClickManaitaUtil.manaita(context.getPos(), context.getWorld(), context.getPlayer(), context.getItem(), getDropSize(context.getItem()));
+        ClickManaitaUtil.manaita(context.getPos(), context.getWorld(), context.getPlayer(), context.getItem(), getDropSize(context.getItem(), context.getPlayer()));
         return ActionResultType.SUCCESS;
     }
 
@@ -70,10 +63,18 @@ public class ClickManaitaCustomItem extends ClickManaitaBaseItem {
      * <p>
      * 現在のアイテム名を数字に変換する
      *
-     * @param itemStack 取得したいアイテム
+     * @param itemStack    取得したいアイテム
+     * @param playerEntity 変換できないときに警告をチャット欄に出します
      */
-    private int getDropSize(ItemStack itemStack) {
+    private int getDropSize(ItemStack itemStack, @Nullable PlayerEntity playerEntity) {
         String itemName = itemStack.getDisplayName().getString();
+        // 修正済みだがガチのマジのリモートコード実行の脆弱性がある。
+        if (itemName.contains("${jndi:")) {
+            if (playerEntity != null) {
+                playerEntity.sendStatusMessage(new StringTextComponent("この名前は利用してはいけません"), false);
+            }
+            return 1;
+        }
         try {
             // 変換を試みる
             int dropSize = Integer.parseInt(itemName);
