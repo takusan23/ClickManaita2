ef53123dac96b09efe8167756ce153a327a7d85a
takusan23 <takumu1200@gmail.com>
2025-10-09 00:01:56 +0900

    mixin 使ってないけど build が通るように

--- src/client/java/io/github/takusan23/clickmanaita/mixin/client/ExampleClientMixin.java
+++ src/client/java/io/github/takusan23/clickmanaita/mixin/client/ExampleClientMixin.java
@@ -1,6 +1,5 @@
 package io.github.takusan23.clickmanaita.mixin.client;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
