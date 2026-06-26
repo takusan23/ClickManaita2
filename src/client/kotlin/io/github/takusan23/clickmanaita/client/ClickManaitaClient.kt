8d1882d1696a51f3b7cf336102592eb5f0aeb18f
takusan23 <takumu1200@gmail.com>
2026-06-27 01:39:06 +0900

    git add もれ

---
+++ src/client/kotlin/io/github/takusan23/clickmanaita/client/ClickManaitaClient.kt
@@ -0,0 +1,9 @@
+package io.github.takusan23.clickmanaita.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object ClickManaitaClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
