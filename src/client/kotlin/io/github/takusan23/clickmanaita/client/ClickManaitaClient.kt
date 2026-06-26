4aed8b899da62b24eccb55852240197538f47df4
takusan23 <takumu1200@gmail.com>
2026-06-27 01:43:04 +0900

    いらないファイルを消す

--- src/client/kotlin/io/github/takusan23/clickmanaita/client/ClickManaitaClient.kt
+++
@@ -1,9 +0,0 @@
-package io.github.takusan23.clickmanaita.client
-
-import net.fabricmc.api.ClientModInitializer
-
-object ClickManaitaClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
